7bc8c4ac3c0e51a12b6b2371f97754fcc885ec98
guest271314 <guest271314@gmail.com>
2022-10-29 19:31:48 -0700

    Process and echo input up to new Array(408)
    new Array(409)  does not yet process correctly or echo.

---
+++ background.js
@@ -0,0 +1,9 @@
+globalThis.name = chrome.runtime.getManifest().short_name;
+globalThis.port = chrome.runtime.connectNative(globalThis.name);
+port.onMessage.addListener((message) => console.log(message));
+port.onDisconnect.addListener((p) => console.log(chrome.runtime.lastError));
+port.postMessage(new Array(408));
+
+chrome.runtime.onInstalled.addListener((reason) => {
+  console.log(reason);
+});
