4e4c28810281f3f9dd1d44f1583233eff641ee96
guest271314 <guest271314@gmail.com>
2022-10-29 19:29:14 -0700

    Process and echo input up to new Array(408)
    new Array(409)  does not yet process correctly or echo.

--- nm_bash.sh
+++ nm_bash.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-# Bash Native Messaging host, guest271314 2022
-# Echo's simple JSON string that does not include (escaped) double quotes
-# How do I use a shell-script as Chrome Native Messaging host application
-# https://stackoverflow.com/a/24777120
+# Loop forever, to deal with chrome.runtime.connectNative
+getMessage() {
+  read -N 1 uint32
+  # https://unix.stackexchange.com/a/13141
+  header=0x$(printf "%s" "$uint32" |
+  od -t x8 -An |
+  tr -dc '[:alnum:]')
+  messageLength=$(printf "%d" "$header")
+  array=()
+  read -N "$messageLength" json
+  array+=("$json")
+  sendMessage "${array[@]}"
+}
 sendMessage() {
-  message=\"$@\"
+  message="$*"
   # Calculate the byte size of the string.
   # NOTE: This assumes that byte length is identical to the string length!
   # Do not use multibyte (unicode) characters, escape them instead, e.g.
@@ -15,60 +24,14 @@ sendMessage() {
   # "Native Messaging host tried sending a message that is ... bytes long.",
   # then just swap the order, i.e. messagelen1 <-> messagelen4 and
   # messagelen2 <-> messagelen3
-  messagelen1=$((($messagelen) & 0xFF))
-  messagelen2=$((($messagelen >> 8) & 0xFF))
-  messagelen3=$((($messagelen >> 16) & 0xFF))
-  messagelen4=$((($messagelen >> 24) & 0xFF))
+  messagelen1=$(((messagelen) & 0xFF))
+  messagelen2=$(((messagelen >> 8) & 0xFF))
+  messagelen3=$(((messagelen >> 16) & 0xFF))
+  messagelen4=$(((messagelen >> 24) & 0xFF))
   # Print the message byte length followed by the actual message.
   printf "$(printf '\\x%x\\x%x\\x%x\\x%x' \
     $messagelen1 $messagelen2 $messagelen3 $messagelen4)%s" "$message"
 }
-
-getMessage() {
-  input=""
-  datacount=0
-  doublequotecount=0
-  
-  # Loop forever, to deal with chrome.runtime.connectNative
-  while true; do
-    IFS= read -res -n1 data
-    # TODO: Process length of JSON (1st $data read).
-    if ((datacount++)); then
-      if [ "$data" == "\"" ]; then
-        if ((doublequotecount++)); then
-          # input+="\\n$args "
-          array+="$input"
-          array+="\\n"
-          array+="$filename"
-          array+="\\n"
-          array+="$args"
-          sendMessage "${array[@]}"
-          # Disconnect host from connectNative client.
-          # break
-        fi
-        continue
-      fi
-      # Do not write double quotation marks.
-      if [ "$data" != "\"" ]; then
-        # Read the first message
-        # Assuming that the message ALWAYS ends with a },
-        # with no }s in the string. Adopt this piece of code if needed.
-        input+="$data"
-      fi
-    fi
-  done
-}
-
-getNativeMessagingHostArguments() {
-  # https://wiki.bash-hackers.org/commands/builtin/caller
-  local frame=0
-  while caller $frame; do
-    ((++frame));
-  done
-  return "$@"
-}
-
-array=()
-filename=`getNativeMessagingHostArguments`
-args="$@"
-getMessage
+while true; do
+  getMessage
+done
